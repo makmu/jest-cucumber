295418b47d4eb07262316bc986ae3e586afea476
Markus Mueller <markus@pyxis.dyndns.org>
2021-03-18 14:40:54 +0100

    refactor

--- src/automatic-step-binding.ts
+++ src/automatic-step-binding.ts
@@ -1,6 +1,11 @@
-import { ParsedFeature } from './models';
+import { ParsedFeature, ScenarioGroup } from './models';
 import { matchSteps } from './validation/step-definition-validation';
-import { StepsDefinitionCallbackFunction, defineFeature, defineRuleBasedFeature } from './feature-definition-creation';
+import {
+    StepsDefinitionCallbackFunction,
+    defineFeature,
+    defineRuleBasedFeature,
+    DefineScenarioFunctionWithAliases
+} from './feature-definition-creation';
 import { generateStepCode } from './code-generation/step-generation';
 
 const globalSteps: Array<{ stepMatcher: string | RegExp; stepFunction: () => any }> = [];
@@ -10,18 +15,53 @@ const registerStep = (stepMatcher: string | RegExp, stepFunction: () => any) => 
 };
 
 const registerSteps = (stepDefinitionCallback: StepsDefinitionCallbackFunction) => {
-        stepDefinitionCallback({
-            defineStep: registerStep,
-            given: registerStep,
-            when: registerStep,
-            then: registerStep,
-            and: registerStep,
-            but: registerStep,
-            pending: () => {
-                // Nothing to do
-            }
+    stepDefinitionCallback({
+        defineStep: registerStep,
+        given: registerStep,
+        when: registerStep,
+        then: registerStep,
+        and: registerStep,
+        but: registerStep,
+        pending: () => {
+            // Nothing to do
+        }
+    });
+};
+
+const matchAndDefineSteps = (feature: ScenarioGroup, test: DefineScenarioFunctionWithAliases, errors: string[]) => {
+    const scenarioOutlineScenarios = feature.scenarioOutlines.map((scenarioOutline) => scenarioOutline.scenarios[0]);
+
+    const scenarios = [ ...feature.scenarios, ...scenarioOutlineScenarios ];
+
+    scenarios.forEach((scenario) => {
+        test(scenario.title, (options) => {
+            scenario.steps.forEach((step, stepIndex) => {
+                const matches = globalSteps.filter((globalStep) => matchSteps(step.stepText, globalStep.stepMatcher));
+
+                if (matches.length === 1) {
+                    const match = matches[0];
+
+                    options.defineStep(match.stepMatcher, match.stepFunction);
+                } else if (matches.length === 0) {
+                    const stepCode = generateStepCode(scenario.steps, stepIndex, false);
+                    // tslint:disable-next-line:max-line-length
+                    errors.push(
+                        `No matching step found for step "${step.stepText}" in scenario "${scenario.title}" in feature "${feature.title}". Please add the following step code: \n\n${stepCode}`
+                    );
+                } else {
+                    const matchingCode = matches.map(
+                        (match) => `${match.stepMatcher.toString()}\n\n${match.stepFunction.toString()}`
+                    );
+                    errors.push(
+                        `${matches.length} step definition matches were found for step "${step.stepText}" in scenario "${scenario.title}" in feature "${feature.title}". Each step can only have one matching step definition. The following step definition matches were found:\n\n${matchingCode.join(
+                            '\n\n'
+                        )}`
+                    );
+                }
+            });
         });
-    }
+    });
+};
 
 export const autoBindSteps = (features: ParsedFeature[], stepDefinitions: StepsDefinitionCallbackFunction[]) => {
     stepDefinitions.forEach(registerSteps);
@@ -30,44 +70,7 @@ export const autoBindSteps = (features: ParsedFeature[], stepDefinitions: StepsD
 
     features.forEach((feature) => {
         defineFeature(feature, (test) => {
-
-            const scenarioOutlineScenarios = feature.scenarioOutlines.map(
-                (scenarioOutline) => scenarioOutline.scenarios[0]
-            );
-
-            const scenarios = [ ...feature.scenarios, ...scenarioOutlineScenarios ];
-
-            scenarios.forEach((scenario) => {
-                test(scenario.title, (options) => {
-                    scenario.steps.forEach((step, stepIndex) => {
-                        const matches = globalSteps.filter((globalStep) =>
-                            matchSteps(step.stepText, globalStep.stepMatcher)
-                        );
-
-                        if (matches.length === 1) {
-                            const match = matches[0];
-
-                            options.defineStep(match.stepMatcher, match.stepFunction);
-                        } else if (matches.length === 0) {
-                            const stepCode = generateStepCode(scenario.steps, stepIndex, false);
-                            // tslint:disable-next-line:max-line-length
-                            errors.push(
-                                `No matching step found for step "${step.stepText}" in scenario "${scenario.title}" in feature "${feature.title}". Please add the following step code: \n\n${stepCode}`
-                            );
-                        } else {
-                            const matchingCode = matches.map(
-                                (match) => `${match.stepMatcher.toString()}\n\n${match.stepFunction.toString()}`
-                            );
-                            errors.push(
-                                `${matches.length} step definition matches were found for step "${step.stepText}" in scenario "${scenario.title}" in feature "${feature.title}". Each step can only have one matching step definition. The following step definition matches were found:\n\n${matchingCode.join(
-                                    '\n\n'
-                                )}`
-                            );
-                        }
-                    });
-                });
-            });
-
+            matchAndDefineSteps(feature, test, errors);
         });
     });
 
@@ -76,55 +79,21 @@ export const autoBindSteps = (features: ParsedFeature[], stepDefinitions: StepsD
     }
 };
 
-export const autoBindStepsWithRules = (features: ParsedFeature[], stepDefinitions: StepsDefinitionCallbackFunction[]) => {
+export const autoBindStepsWithRules = (
+    features: ParsedFeature[],
+    stepDefinitions: StepsDefinitionCallbackFunction[]
+) => {
     stepDefinitions.forEach(registerSteps);
 
     const errors: string[] = [];
 
     features.forEach((feature) => {
-        defineRuleBasedFeature(feature, (rule) => {
-
-          feature.rules.forEach((r) => {
-            rule(r.title, (test) => {
-
-              const scenarioOutlineScenarios = r.scenarioOutlines.map(
-                  (scenarioOutline) => scenarioOutline.scenarios[0]
-              );
-
-              const scenarios = [ ...r.scenarios, ...scenarioOutlineScenarios ];
-
-              scenarios.forEach((scenario) => {
-                  test(scenario.title, (options) => {
-                      scenario.steps.forEach((step, stepIndex) => {
-                          const matches = globalSteps.filter((globalStep) =>
-                              matchSteps(step.stepText, globalStep.stepMatcher)
-                          );
-
-                          if (matches.length === 1) {
-                              const match = matches[0];
-
-                              options.defineStep(match.stepMatcher, match.stepFunction);
-                          } else if (matches.length === 0) {
-                              const stepCode = generateStepCode(scenario.steps, stepIndex, false);
-                              // tslint:disable-next-line:max-line-length
-                              errors.push(
-                                  `No matching step found for step "${step.stepText}" in scenario "${scenario.title}" in feature "${feature.title}". Please add the following step code: \n\n${stepCode}`
-                              );
-                          } else {
-                              const matchingCode = matches.map(
-                                  (match) => `${match.stepMatcher.toString()}\n\n${match.stepFunction.toString()}`
-                              );
-                              errors.push(
-                                  `${matches.length} step definition matches were found for step "${step.stepText}" in scenario "${scenario.title}" in feature "${feature.title}". Each step can only have one matching step definition. The following step definition matches were found:\n\n${matchingCode.join(
-                                      '\n\n'
-                                  )}`
-                              );
-                          }
-                      });
-                  });
-              });
-            })
-          })
+        defineRuleBasedFeature(feature, (ruleDefinition) => {
+            feature.rules.forEach((rule) => {
+                ruleDefinition(rule.title, (test) => {
+                    matchAndDefineSteps(rule, test, errors);
+                });
+            });
         });
     });
 
